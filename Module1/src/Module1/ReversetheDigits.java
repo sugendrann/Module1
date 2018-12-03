98d3d855260bdeab9b497844d41b6c1cbdf6bea9
sugen <sugen@192.168.0.10>
2018-12-03 10:42:19 -0800

    Modified reverseDigits class

--- Module1/src/Module1/ReversetheDigits.java
+++ Module1/src/Module1/ReversetheDigits.java
@@ -12,44 +12,20 @@ public class ReversetheDigits {
 		int n = sc.nextInt();
 		int length = String.valueOf(n).length();
 		System.out.println(length);
-		String result = "";
-	
-		/*while(i<=length)
-		{
-			int temp = (int) (n / (java.lang.Math.pow(10,i)));
-			int temp1 = (int) (n % (java.lang.Math.pow(10,i)));
-			result = (int) (result+(temp1*(java.lang.Math.pow(10,length)));
-			
-		}
-		
-		
-		
-		while (length >= 0) {
-			int temp2 = (int) (n % (java.lang.Math.pow(10,--length)));
-			int temp1 = (int) (n % (java.lang.Math.pow(10,i)));
-			result = result + temp1 * (int) (java.lang.Math.pow(10,length));
-			
-			
-			i++;
-		
-		}
-		System.out.println("Add the digits result=" +result);
-		sc.close();*/
-	
-
+		int result = 0;
+		int i = 1;
 		while (length > 0) {
-			int temp = (int) (n / (java.lang.Math.pow(10,(length-1))));
-			int temp1 = (int) (n % (java.lang.Math.pow(10,length-1)));
-			result =  temp+result;
-			n=temp1;
-		length--;
-		
-		}
-		System.out.println("Add the digits result=" +result);
-sc.close();
 
-	
-		
+			int temp1 = (int) (n % (java.lang.Math.pow(10, i)));
+			result = temp1 + result * 10;
+			n = n / 10;
+			length--;
+
+		}
+
+		System.out.println("Reverse the digits result=" + result);
+		sc.close();
+
 	}
 
 }
